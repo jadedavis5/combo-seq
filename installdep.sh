01305dac99ffe5b99cbd51a5677179c049c072e1
jadedavis5 <111946376+jadedavis5@users.noreply.github.com>
2023-01-31 11:41:20 +0800

    Create installdep.sh

---
+++ installdep.sh
@@ -0,0 +1,8 @@
+        conda install -c -y bioconda star
+        conda install -c -y bioconda fqtrim
+        conda install -c -y bioconda bowtie
+        conda install -c -y bioconda pear
+        conda install -c -y bioconda samtool
+        conda install -c -y bioconda nextflow
+        conda install -c -y bioconda fastqc
+        conda install -c -y bioconda multiqc
