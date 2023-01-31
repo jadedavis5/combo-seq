499cc54f44ae101948da85b8151ff08a058dfc32
jadedavis5 <111946376+jadedavis5@users.noreply.github.com>
2023-01-31 14:47:54 +0800

    Update installdep.sh

--- installdep.sh
+++ installdep.sh
@@ -1,8 +1,8 @@
-        conda install -y -c bioconda star
-        conda install -y -c bioconda fqtrim
-        conda install -y -c bioconda bowtie
-        conda install -y -c bioconda pear
-        conda install -y -c bioconda samtool
-        conda install -y -c bioconda nextflow
-        conda install -y -c bioconda fastqc
-        conda install -y -c bioconda multiqc
+conda install -y -c bioconda star
+conda install -y -c bioconda fqtrim
+sudo apt install bowtie2
+conda install -y -c bioconda pear
+conda install -y -c bioconda samtool
+conda install -y -c bioconda nextflow
+conda install -y -c bioconda fastqc
+conda install -y -c bioconda multiqc
